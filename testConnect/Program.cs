c5d4d5fb5cac7dc7ddb24f511632a7c4bca0f8ea
Oleksiy Boldarev <oboldarev@alegri.eu>
2019-10-11 15:05:02 +0200

    docker Projects added

---
+++ testConnect/Program.cs
@@ -0,0 +1,22 @@
+﻿using LdapForNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var con = new LdapConnection())
+            {
+                con.Connect("docker.host.internal", 50000);
+                con.Bind();
+                Console.WriteLine("OK");
+            }
+        }
+    }
+}
